0ac47225ea0db12370dcf0d98f4bbb904c18a5b8
Neema <neema.edward@student.moringaschool.com>
2025-03-17 10:41:56 +0300

    adds the switch in images at an interval of 2 seconds

--- index.js
+++ index.js
@@ -3,3 +3,23 @@ const ramens = [
     { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "miso.jpg", rating: 4, comment: "Very flavorful!" },
     { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" }
  ];
+
+ document.addEventListener("DOMContentLoaded", () => {
+    const ramensArray = [
+        "Images/gyukotsu.jpg",
+        "Images/kojiro.jpg",
+        "Images/naruto.jpg",
+        "Images/nirvana.jpg",
+        "Images/shoyu.jpg",
+    ];
+
+    const im2 = document.getElementById("im2");
+    let currentIndex = 0;
+
+    function switchImage() {
+        currentIndex = (currentIndex + 1) % ramensArray.length;
+        im2.src = ramensArray[currentIndex]
+    }
+
+    setInterval(switchImage, 2000);
+ });
