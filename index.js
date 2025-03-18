bdc7ab7b2aab6f0dc43a81876b8329e748893805
Neema <neema.edward@student.moringaschool.com>
2025-03-18 09:13:39 +0300

    adding elements

--- index.js
+++ index.js
@@ -1,43 +1,49 @@
 const ramens = [
-    { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "shoyu.jpg", rating: 5, comment: "Delicious!" },
-    { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "miso.jpg", rating: 4, comment: "Very flavorful!" },
-    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" }
+    { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "Images/shoyu.jpg", rating: 5, comment: "Delicious!" },
+    { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "Images/kojiro.jpg", rating: 4, comment: "Very flavorful!" },
+    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "Images/gyukotsu.jpg" },
+    {id: 4, name: "Naruto Ramen", restaurant: "Ramen-ya", image: "Images/naruto.jpg" },
+    {id: 3, name: "Neema Ramen", restaurant: "Ramen-ya", image: "Images/nirvana.jpg" }
  ];
 
- document.addEventListener("DOMContentLoaded", () => {
-    const ramensArray = [
-        "Images/gyukotsu.jpg",
-        "Images/kojiro.jpg",
-        "Images/naruto.jpg",
-        "Images/nirvana.jpg",
-        "Images/shoyu.jpg",
-    ];
+//  document.addEventListener("DOMContentLoaded", () => {
+//     const ramensArray = [
+//         "Images/gyukotsu.jpg",
+//         "Images/kojiro.jpg",
+//         "Images/naruto.jpg",
+//         "Images/nirvana.jpg",
+//         "Images/shoyu.jpg",
+//     ];
 
-    const img = document.getElementById("im2");
-    let currentIndex = 0;
+//     const img = document.getElementById("im2");
+//     let currentIndex = 0;
 
-    function switchImage() {
-        currentIndex = (currentIndex + 1) % ramensArray.length;
-        im2.src = ramensArray[currentIndex]
-    }
+//     function switchImage() {
+//         currentIndex = (currentIndex + 1) % ramensArray.length;
+//         im2.src = ramensArray[currentIndex]
+//     }
 
-    setInterval(switchImage, 2000);
+//     setInterval(switchImage, 2000);
  
 
-    displayRamens();
-});
+//     displayRamens();
+// });
 
+const ramenMenu =document.getElementById("ramen-menu");
+const placeholder = document.getElementById('im2');
  function displayRamens() {
-    const ramenMenu =document.getElementById("ramen-menu");
-    ramens.forEach((ramens) => {
-        const img =document.createElement("im2");
-        img.src =ramens.img;
-        img.alt = ramens.name;
-        img.addEventListener("click", () => handleClick(ramens));
-        ramenMenu.appendChild(img);
-    });
+    
+    
+    ramenMenu.addEventListener('click', function(event){
+        
+        if(event.target.classList.contains('image-container')){
+            placeholder.src = event.target.src;
+            placeholder.alt = event.target.alt;
+        }
+    })
+
  }
-    // displayRamens();
+  
 
     // function addSubmitListener() {
     //     const form = document.getElementById('ramen-form');
@@ -57,5 +63,8 @@ const ramens = [
     //     });
     // }
  
+    (function main(){
+        displayRamens();
+    })();
 
 
