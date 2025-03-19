6b4275bd7bc49c0444983c2609406ef3fdc94547
Neema <neema.edward@student.moringaschool.com>
2025-03-19 12:09:23 +0300

    final commit

--- index.js
+++ index.js
@@ -6,28 +6,7 @@ const ramens = [
     {id: 5, name: "Neema Ramen", restaurant: "Ramen-ya", image: "Images/nirvana.jpg", rating: 5, comment: "Fabulous" }
  ];
 
-//  document.addEventListener("DOMContentLoaded", () => {
-//     const ramensArray = [
-//         "Images/gyukotsu.jpg",
-//         "Images/kojiro.jpg",
-//         "Images/naruto.jpg",
-//         "Images/nirvana.jpg",
-//         "Images/shoyu.jpg",
-//     ];
 
-//     const img = document.getElementById("im2");
-//     let currentIndex = 0;
-
-//     function switchImage() {
-//         currentIndex = (currentIndex + 1) % ramensArray.length;
-//         im2.src = ramensArray[currentIndex]
-//     }
-
-//     setInterval(switchImage, 2000);
- 
-
-//     displayRamens();
-// });
 
 const ramenMenu =document.getElementById("ramen-menu");
 const placeholder = document.getElementById('im2');
@@ -35,49 +14,31 @@ const imageRating = document.getElementById('rating-p');
 const imageComment = document.getElementById('comment-p');
  
 function displayRamens() {
-    for(let element of ramens){
+    for(let a of ramens){
         let createdImage = document.createElement('img');
-        createdImage.src = element.image;
-        createdImage.alt = element.name;
+        createdImage.src = a.image;
+        createdImage.alt = a.name;
         createdImage.classList.add('image-container');         
         
         
         ramenMenu.appendChild(createdImage);
     }
-    
+
     ramenMenu.addEventListener('click', function(event){
-        
-        if(event.target.classList.contains('image-container')){
+        if (event.target.className === "image-container") {
             placeholder.src = event.target.src;
             placeholder.alt = event.target.alt;
 
-            imageRating.innerText = `${element.rating}/5`;
-            imageComment.innerText = element.comment;
-        }
+            let exact = ramens.find(a => event.target.src.slice(22) === a.image);
+            
+            imageRating.textContent = exact.rating + "/5"
+            imageComment.textContent = exact.comment
+        };
+        
     })
 
  }
-  
-
-    // function addSubmitListener() {
-    //     const form = document.getElementById('ramen-form');
-    //     form.addEventListener('submit', (event) => {
-    //         event.preventDefault(); 
-    //         const newRamen = {
-    //             id: ramens.length + 1, 
-    //             name: form.name.value,
-    //             restaurant: form.restaurant.value,
-    //             image: form.image.value,
-    //             rating: parseInt(form.rating.value) || null,
-    //             comment: form.comment.value || ''
-    //         };
-    //         ramens.push(newRamen); 
-    //         displayRamens(); 
-    //         form.reset(); 
-    //     });
-    // }
- 
-    (function main(){
+  (function main(){
         displayRamens();
     })();
 
